d614e111f3957475878d363f67c45a4d74fcb4ad
Asgeir Nilsen <asgeir@fintprosjektet.no>
2017-09-19 15:38:02 +0200

    Workaround for code generation bug.

--- src/main/java/no/fint/model/vigokodeverk/Grep.java
+++ src/main/java/no/fint/model/vigokodeverk/Grep.java
@@ -5,11 +5,9 @@ package no.fint.model.vigokodeverk;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode
 public class Grep {}
 
