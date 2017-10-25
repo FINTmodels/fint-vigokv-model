bd80111459ed29fe40eb7a2029ce933856b4d62e
jarlehansen <jarle@jarlehansen.net>
2017-10-25 21:24:45 +0200

    added snapshot testing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # fint-vigokv-model
+
+
+## Snapshot tester
+
+Testene er tilgjengelig i `ModelSpec`. De genererte json-filene vil lagres i `src/test/resources/snapshots`, disse skal sjekkes inn i git.
+
+1. Kjør testen `Matches snapshots` for å verifisere at modellene fungerer med snapshot filene.
+2. Kjør `Create snapshots` for å regenerere snapshot json-filene. Denne testen må kjøres med system property `CREATE_SNAPSHOTS=true`.
---
+++ src/test/groovy/no/fint/model/vigokodeverk/ModelSpec.groovy
@@ -0,0 +1,25 @@
+package no.fint.model.vigokodeverk
+
+import no.fint.model.test.utils.JsonSnapshots
+import spock.lang.Requires
+import spock.lang.Specification
+
+class ModelSpec extends Specification {
+    private JsonSnapshots jsonSnapshots
+
+    void setup() {
+        jsonSnapshots = new JsonSnapshots(this)
+    }
+
+    @Requires({ Boolean.valueOf(sys['CREATE_SNAPSHOTS']) })
+    def "Create snapshots"() {
+        expect:
+        jsonSnapshots.create()
+    }
+
+    def "Matches snapshots"() {
+        expect:
+        jsonSnapshots.matchesSnapshots()
+        jsonSnapshots.matchesRelationNames()
+    }
+}
